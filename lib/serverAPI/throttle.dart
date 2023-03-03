24fb343d3384cc8ea02a96f3b476788f01b3b7fe
Lulero <lulero.gaming@gmail.com>
2023-03-03 18:21:52 +0100

    Force the use of Throttler.instance()

--- lib/serverAPI/throttle.dart
+++ lib/serverAPI/throttle.dart
@@ -1,6 +1,8 @@
 class Throttler {
   DateTime _nextTime = DateTime(0);
 
+  Throttler._();
+
   Future<void> throttle(Duration nextDelay) async {
     final DateTime now = DateTime.now();
     if (now.isBefore(_nextTime)) {
@@ -9,5 +11,5 @@ class Throttler {
     _nextTime = DateTime.now().add(nextDelay);
   }
 
-  static final Throttler instance = Throttler();
+  static final Throttler instance = Throttler._();
 }
